85fa8c33f911fec5e1b625713a19a1fa9deaeeef
Jochen Jonckheere <jochen.jonckheere@jxk.be>
2018-02-13 21:00:36 +0100

    Introducing local storage

---
+++ JxK.HomeAutomation/Controllers/SettingsController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Windows.Storage;
+
+namespace JxK.HomeAutomation.Controllers
+{
+    internal class SettingsController
+    {
+        ApplicationDataContainer _localSettings;
+
+        public SettingsController()
+        {
+            _localSettings = ApplicationData.Current.LocalSettings;
+        }
+
+        public DateTimeOffset? PreviousTime
+        {
+            get => _localSettings.Values["#PreviousTime"] as DateTimeOffset?;
+            set => _localSettings.Values["#PreviousTime"] = value;
+        }
+
+        public DateTimeOffset? LastOpeningTime
+        {
+            get => _localSettings.Values["#LastOpeningTime"] as DateTimeOffset?;
+            set => _localSettings.Values["#LastOpeningTime"] = value;
+        }
+
+        public DateTimeOffset? LastClosingTime
+        {
+            get => _localSettings.Values["#LastClosingTime"] as DateTimeOffset?;
+            set => _localSettings.Values["#LastClosingTime"] = value;
+        }
+    }
+}
--- JxK.HomeAutomation/StartupTask.cs
+++ JxK.HomeAutomation/StartupTask.cs
@@ -6,6 +6,7 @@ using Windows.Devices.Gpio;
 using Windows.System.Threading;
 using Innovative.SolarCalculator;
 
+
 namespace JxK.HomeAutomation
 {
     public sealed class StartupTask : IBackgroundTask
@@ -14,6 +15,7 @@ namespace JxK.HomeAutomation
         private Geoposition _position;
         private VeluxController _veluxController;
         private MailController _mailController;
+        private SettingsController _settingsController;
         private ThreadPoolTimer _timer;
 
         public async void Run(IBackgroundTaskInstance taskInstance)
@@ -39,9 +41,11 @@ namespace JxK.HomeAutomation
                 SubjectPrefix = "[HomeAutomation]"
             };
 
-            // Setup a simple timer for testing/demo purposes
-            _timer = ThreadPoolTimer.CreatePeriodicTimer(Timer_Tick, TimeSpan.FromMinutes(30));
+            // Create a Settings Controller
+            _settingsController = new SettingsController();
 
+            // Setup a simple timer for testing/demo purposes
+            _timer = ThreadPoolTimer.CreatePeriodicTimer(Timer_Tick, TimeSpan.FromMinutes(15));
 
             // IDEA: Use UWP to register a timed event
             //BackgroundTaskRegistration task = RegisterBackgroundTask(entryPoint, taskName, hourlyTrigger, userCondition);
@@ -57,35 +61,53 @@ namespace JxK.HomeAutomation
         {
             // Get the current time
             var currentTime = DateTimeOffset.Now;
-            var previousTime = currentTime.Subtract(timer.Period);
+
+            // Get the previous time the timer ticked
+            var previousTime = _settingsController.PreviousTime ?? currentTime.Subtract(timer.Period);
             
-            // Calculate sunrise & sunset (since last time the timed ticked)
+            // Calculate sunrise & sunset for the current day (since last time the timer ticked)
             var solarTimes = new SolarTimes(previousTime, _position.Coordinate.Point.Position.Latitude, _position.Coordinate.Point.Position.Longitude);
             var sunrise = new DateTimeOffset(solarTimes.Sunrise);
             var sunset = new DateTimeOffset(solarTimes.Sunset);
 
+
+            // Has the sun already risen today?
+            // => No, do nothing
+            // => Yes and is it after 7h45 on a working day?
+            //    => No, do nothing
+            //    => Yes and have we already opened the blinds today?
+            //       => No, open the blinds
+            //       => Yes, do nothing
+
+            // Has the sun already set today?
+            // => No, do nothing
+            // => Yes and have we already closed the blinds today?
+            //    => No, close the blinds
+            //    => Yes, do nothing
+
+
             // Check if the sun has set since the last time the timer ticked
             if (previousTime <= sunset && sunset < currentTime)
             {
-                _mailController.Send("Closing the Velux blinds", $"Sunset: {sunset.ToString("F")}\r\nCurrent time: {currentTime.ToString("F")}\r\nPrevious time: {previousTime.ToString("F")}\r\n\r\nLocation: http://www.google.com/maps/place/{_position.Coordinate.Point.Position.Latitude},{_position.Coordinate.Point.Position.Longitude}");
+                _mailController.Send("Closing the Velux blinds", $"Sunset: {sunset.ToString("F")}\r\n\r\nCurrent time: {currentTime.ToString("F")}\r\nPrevious time: {previousTime.ToString("F")}\r\n\r\nLocation: http://www.google.com/maps/place/{_position.Coordinate.Point.Position.Latitude},{_position.Coordinate.Point.Position.Longitude}");
                 _veluxController.Down();
             }
+            // Check if the sun has rised since the last time the timer ticked
+            //  + it's a working day (= weekday)
+            //  + I'm already up (= after 7h30)
+            //else if (previousTime <= sunrise && sunrise < currentTime)
+            //{
+            //    _mailController.Send("Opening the Velux blinds", $"Sunrise: {sunrise.ToString("F")}\r\nCurrent time: {currentTime.ToString("F")}\r\nPrevious time: {previousTime.ToString("F")}\r\n\r\nLocation: http://www.google.com/maps/place/{_position.Coordinate.Point.Position.Latitude},{_position.Coordinate.Point.Position.Longitude}");
+            //    _veluxController.Up();
+            //}
             //else
             //{
             //    _mailController.Send($"Logging all parameters ({currentTime.ToString("dd/HH")})", $"Sunset: {sunset.ToString("F")}\r\nCurrent time: {currentTime.ToString("F")}\r\nPrevious time: {previousTime.ToString("F")}\r\n\r\nLocation: http://www.google.com/maps/place/{_position.Coordinate.Point.Position.Latitude},{_position.Coordinate.Point.Position.Longitude}");
             //    _veluxController.Stop();
             //}
 
-            // 
-
-            //_veluxController.Up();
-            //Task.Delay(-1).Wait(2000);
-
-            //_veluxController.Stop();
-            //Task.Delay(-1).Wait(2000);
-
-            //_veluxController.Down();
-            //Task.Delay(-1).Wait(2000);
+            // Save the settings
+            _settingsController.PreviousTime = currentTime;
         }
     }
 }
