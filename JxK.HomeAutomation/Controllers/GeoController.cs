b900ec9c0aa7f0df8db1ae7b0b15d55dde1657ff
Jochen Jonckheere <jochen.jonckheere@jxk.be>
2018-05-27 12:29:10 +0200

    Work with a fixed location

---
+++ JxK.HomeAutomation/Controllers/GeoController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Threading.Tasks;
+using Windows.ApplicationModel.Resources;
+using Windows.Devices.Geolocation;
+
+namespace JxK.HomeAutomation.Controllers
+{
+    internal class GeoController
+    {
+        private readonly ResourceLoader _resourceLoader;
+        private readonly Geolocator _geolocator;
+
+        private bool _autoDetect;
+        private double _latitude;
+        private double _longitude;
+
+        public GeoController()
+        {
+            _resourceLoader = ResourceLoader.GetForViewIndependentUse("GeoResources");
+
+            _geolocator = new Geolocator();
+
+            bool.TryParse(_resourceLoader.GetString("AutoDetect"), out _autoDetect);            
+            double.TryParse(_resourceLoader.GetString("Latitude"), out _latitude);
+            double.TryParse(_resourceLoader.GetString("Longitude"), out _longitude);
+        }
+
+        public async Task<GeoCoordinate> GetGeoCoordinateAsync()
+        {
+            if (_autoDetect)
+            {
+                var geoposition = await _geolocator.GetGeopositionAsync();
+
+                return new GeoCoordinate(geoposition.Coordinate.Point.Position.Latitude, geoposition.Coordinate.Point.Position.Longitude);
+            }
+            else
+            {
+                return new GeoCoordinate(_longitude, _latitude);
+            }
+        }
+
+        public class GeoCoordinate
+        {
+            public double Latitude { get; }
+            public double Longitude { get; }
+
+            public GeoCoordinate(double latitude, double longitude)
+            {
+                Latitude = latitude;
+                Longitude = longitude;
+            }
+        }
+    }
+}
--- JxK.HomeAutomation/StartupTask.cs
+++ JxK.HomeAutomation/StartupTask.cs
@@ -1,17 +1,17 @@
 ﻿using JxK.HomeAutomation.Controllers;
 using System;
 using Windows.ApplicationModel.Background;
-using Windows.Devices.Geolocation;
 using Windows.Devices.Gpio;
 using Windows.System.Threading;
 using Innovative.SolarCalculator;
+using static JxK.HomeAutomation.Controllers.GeoController;
 
 namespace JxK.HomeAutomation
 {
     public sealed class StartupTask : IBackgroundTask
     {
         private BackgroundTaskDeferral _deferral;
-        private Geoposition _position;
+        private GeoCoordinate _geoCoordinate;
         private VeluxController _veluxController;
         private MailController _mailController;
         private SettingsController _settingsController;
@@ -25,11 +25,11 @@ namespace JxK.HomeAutomation
             // Get notified when this task is being terminated by the OS
             taskInstance.Canceled += TaskInstance_Canceled;
 
-            // Create geolocator object
-            var geolocator = new Geolocator();
+            // Create a Goe Controller
+            var geolocator = new GeoController();
 
             // Make the request for the current position
-            _position = await geolocator.GetGeopositionAsync();
+            _geoCoordinate = await geolocator.GetGeoCoordinateAsync();
 
             // Create a Velux Controller
             _veluxController = new VeluxController(GpioController.GetDefault());
@@ -62,16 +62,16 @@ namespace JxK.HomeAutomation
             var currentTime = DateTimeOffset.Now;
 
             // Margin (there is already light before sunrise and there is still light after sunset)
-            var margin = TimeSpan.FromMinutes(20);
+            var margin = TimeSpan.FromMinutes(15);
 
             // Calculate sunrise & sunset for the current day
-            var solarTimes = new SolarTimes(currentTime, _position.Coordinate.Point.Position.Latitude, _position.Coordinate.Point.Position.Longitude);
+            var solarTimes = new SolarTimes(currentTime, _geoCoordinate.Latitude, _geoCoordinate.Longitude);
             var sunrise = new DateTimeOffset(solarTimes.Sunrise);
             var sunset = new DateTimeOffset(solarTimes.Sunset);
 
             // Has the sun already risen today (minus the margin)?
             // => No, do nothing
-            // => Yes and is it after 7h45 on a working day?
+            // => Yes and is it after 7h30 on a working day?
             //    => No, do nothing
             //    => Yes and have we already opened the blinds today?
             //       => No, open the blinds
@@ -79,11 +79,11 @@ namespace JxK.HomeAutomation
             if (sunrise.Subtract(margin) < currentTime)
             {
                 // TODO: Add holidays
-                if (currentTime.DayOfWeek != DayOfWeek.Saturday && currentTime.DayOfWeek != DayOfWeek.Sunday && currentTime.TimeOfDay >= new TimeSpan(7, 45, 0))
+                if (currentTime.DayOfWeek != DayOfWeek.Saturday && currentTime.DayOfWeek != DayOfWeek.Sunday && currentTime.TimeOfDay >= new TimeSpan(7, 30, 0))
                 {
                     if (_settingsController.LastOpeningTime == null || _settingsController.LastOpeningTime.Value.Date != currentTime.Date)
                     {
-                        var sendMailResult = _mailController.Send("Opening the Velux blinds", $"Sunrise: {sunrise.ToString("F")}\r\nMargin: {margin.ToString("%m' min.'")}\r\n\r\nCurrent time: {currentTime.ToString("F")}\r\n\r\nLocation: http://www.google.com/maps/place/{_position.Coordinate.Point.Position.Latitude},{_position.Coordinate.Point.Position.Longitude}");
+                        var sendMailResult = _mailController.Send("Opening the Velux blinds", $"Sunrise: {sunrise.ToString("F")}\r\nMargin: {margin.ToString("%m' min.'")}\r\n\r\nCurrent time: {currentTime.ToString("F")}\r\n\r\nLocation: http://www.google.com/maps/place/{_geoCoordinate.Latitude},{_geoCoordinate.Longitude}");
 
                         _veluxController.Up();
                         _settingsController.LastOpeningTime = currentTime;
@@ -106,7 +106,7 @@ namespace JxK.HomeAutomation
             {
                 if (_settingsController.LastClosingTime == null || _settingsController.LastClosingTime.Value.Date != currentTime.Date)
                 {
-                    var sendMailResult = _mailController.Send("Closing the Velux blinds", $"Sunset: {sunset.ToString("F")}\r\nMargin: {margin.ToString("%m' min.'")}\r\n\r\nCurrent time: {currentTime.ToString("F")}\r\n\r\nLocation: http://www.google.com/maps/place/{_position.Coordinate.Point.Position.Latitude},{_position.Coordinate.Point.Position.Longitude}");
+                    var sendMailResult = _mailController.Send("Closing the Velux blinds", $"Sunset: {sunset.ToString("F")}\r\nMargin: {margin.ToString("%m' min.'")}\r\n\r\nCurrent time: {currentTime.ToString("F")}\r\n\r\nLocation: http://www.google.com/maps/place/{_geoCoordinate.Latitude},{_geoCoordinate.Longitude}");
 
                     _veluxController.Down();
                     _settingsController.LastClosingTime = currentTime;
